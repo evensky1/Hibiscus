c5a2bc4adaebe70e453fb8676be99e41cc09357f
evensky1 <fromthedeepsize@gmail.com>
2022-05-28 21:20:27 +0300

    added exception

---
+++ src/main/java/com/poit/hibiscus/error/factory/model/CardException.java
@@ -0,0 +1,7 @@
+package com.poit.hibiscus.error.factory.model;
+
+public class CardException extends RuntimeException {
+    public CardException(String message) {
+        super(message);
+    }
+}
